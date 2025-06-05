20ec095f9a28a14c321a862e24e3b2e2f5dbf64c
errishabhsrivastava <ersrivastavarishabh@gmail.com>
2025-06-05 20:38:08 +0530

    v0.0.2

--- README.md
+++
@@ -1,2 +0,0 @@
-# GWS365 Shared Models
-This contains shared models like User, Payment etc. for reuse across multiple subdomains.
--- middlewares/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  authMiddleware: require('./authMiddleware'),
-  checkBilling: require('./checkBilling'),
-  hasPermission: require('./hasPermission'),
-  navbarMiddleware: require('./navbarMiddleware'),
-};
