087eb063f3557a09e852d15213da4f967d3bf91d
errishabhsrivastava <ersrivastavarishabh@gmail.com>
2025-06-05 20:36:15 +0530

    v0.0.0

---
+++ middlewares/navbarMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
+const navbarMiddleware = async (req, res, next) => {
+  const token = req.cookies.gwsToken;
+
+  if (!token) return res.redirect('/login');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+    if (userId) {
+        // Attach user info to requestx
+      const messagesRes = await axios.get(process.env.Login_URL + `/api/v1.0/client/`);
+      req.products = messagesRes.data || []; // attach messages
+
+       // Get notifications
+    //const notifRes = await axios.get(`https://auth.gws365.in/api/v1.0/notification/${userId}`);
+    //req.notifications = notifRes.data.notifications || [];
+
+      next();
+    } else {
+      return res.redirect('/login');
+    }
+
+  } catch (err) {
+    console.error('Auth Middleware Error:', err);
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = navbarMiddleware;
