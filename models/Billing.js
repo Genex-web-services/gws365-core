92f3d30b013d7dd1b02ab19cffc8e02d6a567ddb
Rishabh Srivastava <117577787+srivastavarishabh17@users.noreply.github.com>
2025-06-05 14:29:05 +0530

    Update Billing.js

--- models/Billing.js
+++ models/Billing.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const billingSchema = new mongoose.Schema({
   tenantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', required: true },
-  planId: { type: mongoose.Schema.Types.ObjectId, ref: 'Plan', required: true },
+  planId: { type: String, required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
   currentCycleStart: Date,
   currentCycleEnd: Date,
